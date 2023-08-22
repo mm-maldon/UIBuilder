473efad77b028d3043a52f043d302cca0ddeda73
Luciano Loma Lorenzana <lucianolomal@gmail.com>
2023-08-21 17:07:09 -0700

    Went a bit crazy, but I think I'm almost done setting up the class for both the genome and the UI

--- AI_Python_Genome_Attributes/genome.py
+++ AI_Python_Genome_Attributes/genome.py
@@ -26,6 +26,7 @@ class ChildGenome:
         self.assets = dict()  # This will hold the assets and their heirarchal value
         self.gene_level = 0  #  The current childs genetic value, the value in which to select valid offspring
         self.assets_len = 0
+        self.parents = tuple()
 
         self.grid = list()
         for _ in range(height):
@@ -61,9 +62,111 @@ class ChildGenome:
     """
 
     def mutate(self):
-        for item, val in self.assets.items():
-            rand_val = random.randint(1, self.assets_len)
-            self.assets[item] = round((val * rand_val), 2)
+        if len(self.parents) != 2:
+            for item, val in self.assets.items():
+                rand_val = random.randint(1, self.assets_len)
+                self.assets[item] = round((val * rand_val), 2)
+        else:
+            parent_A = self.parents[0].get_assets()
+            parent_B = self.parents[1].get_assets()
+
+            for item, value_A in parent_A.items():
+                value_B = parent_B[item]
+                self.assets[item] = round(((value_A + value_B) / 2), 2)
+
+    # Access Functions ------------------------------------------------------
+
+    """
+    Literally just loads parents into the new child
+    """
+
+    def load_parents(self, new_parents):
+        self.parents = new_parents
+
+    """
+    An access function to get the parents assets dicitonary with their unique values
+    """
+
+    def get_assets(self):
+        return self.assets
+
+    """
+    A helper function that can be called to return the asset with the highest value
+    """
+
+    def max_asset(self):
+        max_value = 0 - 1
+        max_item = None
+        for item, value in self.assets.items():
+            if value > max_value:
+                max_item = item
+
+        if max_value > (0 - 1):
+            return max_item
+        else:
+            return False
+
+    # Template Functions ----------------------------------------------------
+
+    """
+    This is a function that places items within the range of the upper left coordinate of the screen
+    """
+
+    def upper_left(self, amount):
+        for _ in range(amount):
+            rand = random.randint(1, 100)
+
+            for Y in range(int(height / 2)):
+                for X in range(int(width / 2)):
+                    if self.grid[Y][X] == "-":
+                        item = self.max_asset()
+                        if (item != False) and (rand < 50):
+                            self.grid[Y][X] = item
+                            break
+                    break
+                break
+
+    def upper_right(self, amount):
+        for _ in range(amount):
+            rand = random.randint(1, 100)
+
+            for Y in range(int(height / 2)):
+                for X in range(int(width / 2), width):
+                    if self.grid[Y][X] == "-":
+                        item = self.max_asset()
+                        if (item != False) and (rand < 50):
+                            self.grid[Y][X] = item
+                            break
+                    break
+                break
+
+    def lower_right(self, amount):
+        for _ in range(amount):
+            rand = random.randint(1, 100)
+
+            for Y in range(int(height / 2), height):
+                for X in range(int(width / 2), width):
+                    if self.grid[Y][X] == "-":
+                        item = self.max_asset()
+                        if (item != False) and (rand < 50):
+                            self.grid[Y][X] = item
+                            break
+                    break
+                break
+
+    def lower_left(self, amount):
+        for _ in range(amount):
+            rand = random.randint(1, 100)
+
+            for Y in range(int(height / 2), height):
+                for X in range(int(width / 2)):
+                    if self.grid[Y][X] == "-":
+                        item = self.max_asset()
+                        if (item != False) and (rand < 50):
+                            self.grid[Y][X] = item
+                            break
+                    break
+                break
 
     # Debugging Functions ---------------------------------------------------
 
--- AI_Python_Genome_Attributes/genome_client.py
+++ AI_Python_Genome_Attributes/genome_client.py
@@ -1,8 +1,21 @@
-# from genome import ChildGenome
+from genome import ChildGenome
 from interface import UI_Grid
 
-UI = UI_Grid()
+UI_A = UI_Grid()
+UI_A.generate_initial_children()
 # print(UI)
-UI.load_assets(["a", "b", "c", "d", "e"])
+UI_A.load_assets(["a", "b", "c", "d", "e"])
 
-UI.print_child_assets()
+# UI_A.print_child_assets()
+
+# print("Mutate Children\n")
+UI_A.mutate_children()
+# UI_A.print_child_assets()
+
+
+print("New Children\n")
+new_generation = UI_A.generate_new_children()
+
+UI_B = UI_Grid()
+UI_B.load_children(new_generation)
+UI_B.print_child_assets()
