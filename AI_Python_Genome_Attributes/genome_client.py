db2c023d2614729482101ceeec980fb3d0187080
Luciano Loma Lorenzana <lucianolomal@gmail.com>
2023-08-27 17:21:50 -0700

    Updated the client file

--- AI_Python_Genome_Attributes/genome_client.py
+++ AI_Python_Genome_Attributes/genome_client.py
@@ -1,31 +1,34 @@
 from genome import ChildGenome
 from interface import UI_Grid
 
+# UI_A.load_assets(["C", "HB", "LOT", "FA", "WL"])
+"""
+Getting user input to create am initial set of children
+"""
+numAssets = int(input("Number of assets: "))
+assetList = list()
+for _ in range(numAssets):
+    tmp = input("Load Asset: ")
+    assetList.append(tmp)
 UI_A = UI_Grid()
 UI_A.generate_initial_children()
-# print(UI_A)
-UI_A.load_assets(["C", "HB", "LOT", "FA", "WL"])
-
-# UI_A.print_child_assets()
-
-print("Mutate Children\n")
+UI_A.load_assets(assetList)
 UI_A.mutate_children()
-# UI_A.print_child_assets()
-
 UI_A.create_grids()
-# print(UI_A)
 
-print("New Children\n")
+
 new_generation = UI_A.generate_new_children()
 UI_B = UI_Grid()
 UI_B.load_children(new_generation)
 UI_B.mutate_children()
 UI_B.create_grids()
-# UI_B.print_child_assets()
 
 for child in UI_B.get_children():
     UI_B.calc_fitness(child)
-    print("Fit: ", child.get_fitness())
-# print(UI_B)
-# UI_B.print_child_assets()
 UI_B.grid_to_txt()
+
+for _ in range(3):
+    """
+    Your Code Here
+    """
+    pass
